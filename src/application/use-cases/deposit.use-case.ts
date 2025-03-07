8588f6e871d74c60f987c7af2ee8be5fc053810d
André Vale <andre_ramosdovale@outlook.com>
2025-03-07 15:42:02 -0300

    fix: ajuste em logica

--- src/application/use-cases/deposit.use-case.ts
+++ src/application/use-cases/deposit.use-case.ts
@@ -1,16 +1,23 @@
-import { AccountRepository } from '../interfaces/account-repository.interface';
 import { Account } from '../../domain/entities/account.entity';
+import { AccountRepository } from '../interfaces/account-repository.interface';
 
 export class DepositUseCase {
   constructor(private accountRepository: AccountRepository) {}
 
   execute(accountId: string, amount: number): Account {
+    if (amount <= 0) {
+      throw new Error('Invalid deposit amount');
+    }
+
     let account = this.accountRepository.getAccountById(accountId);
+
     if (!account) {
       account = new Account(accountId);
     }
+
     account.deposit(amount);
     this.accountRepository.saveAccount(account);
+
     return account;
   }
 }
